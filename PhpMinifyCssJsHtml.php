4288e443aad6c97d197224f93b00e9c2e85e80de
ryanbriscall <ryanbriscall@gmail.com>
2025-03-07 16:30:34 -0700

    add PhpMinifyCssJsHtml.php

---
+++ PhpMinifyCssJsHtml.php
@@ -0,0 +1,138 @@
+function minifyCSS($css) {
+	preg_match_all('/(\"[^\"]*\"|\'[^\']*\'|url\([^)]+\))/', $css, $matches);
+	$placeholders = [];
+	
+	foreach ($matches[0] as $i => $match) {
+		$placeholder = "__CSS_STRING_".$i."__";
+		$css = str_replace($match, $placeholder, $css);
+		$placeholders[$placeholder] = $match;
+	}
+
+	$css = preg_replace('/\s*{\s*/', '{', $css);
+	$css = preg_replace('/\s*}\s*/', '}', $css);
+	$css = preg_replace('/\s*:\s*/', ':', $css);
+	$css = preg_replace('/\s*;\s*/', ';', $css);
+	$css = preg_replace('/;}/', '}', $css);
+	$css = preg_replace('/\s+/', ' ', $css);
+	$css = trim($css);
+
+	foreach ($placeholders as $placeholder => $original) {
+		$css = str_replace($placeholder, $original, $css);
+	}
+
+	return str_replace("\n", '', $css);
+}
+
+function minifyJS($js) {
+	preg_match_all('/(\"[^\"]*\"|\'[^\']*\'|`[^`]*`)/', $js, $matches);
+	$placeholders = [];
+	
+	foreach ($matches[0] as $i => $match) {
+		$placeholder = "__JS_STRING_".$i."__";
+		$js = str_replace($match, $placeholder, $js);
+		$placeholders[$placeholder] = $match;
+	}
+
+	$js = preg_replace('/\s*([{}();,:+<>=|&!])\s*/', '$1', $js);
+	$js = preg_replace('/;\}/', '}', $js);
+	$js = preg_replace('/\s+/', ' ', $js);
+	$js = trim($js);
+
+	foreach ($placeholders as $placeholder => $original) {
+		if ($original[0] === '`') {
+			$htmlContent = substr($original, 1, -1);
+			
+			$htmlContent = preg_replace_callback(
+				'/<(pre|textarea|code)[^>]*>.*?<\/\1>/is',
+				function ($matches) {
+					return base64_encode($matches[0]);
+				},
+				$htmlContent
+			);
+
+			$htmlContent = preg_replace('/\s*>\s*/', '>', $htmlContent);
+			$htmlContent = preg_replace('/\s*</', '<', $htmlContent);
+
+			$htmlContent = preg_replace_callback(
+				'/[A-Za-z0-9+\/=]+/',
+				function ($matches) {
+					return base64_decode($matches[0]);
+				},
+				$htmlContent
+			);
+
+			$original = "`" . $htmlContent . "`";
+		}
+		$js = str_replace($placeholder, $original, $js);
+	}
+
+	return str_replace("\n", '', $js);
+}
+
+function minifyHTML($html) {
+	$placeholders = [];
+
+	$html = preg_replace_callback(
+		'/<(script|style)(\b[^>]*)>.*?<\/\1>/is',
+		function ($matches) use (&$placeholders) {
+			if (strpos($matches[2], 'src=') !== false || strpos($matches[2], 'href=') !== false) {
+				return $matches[0];
+			}
+
+			$key = "__PLACEHOLDER_" . count($placeholders) . "__";
+			$placeholders[$key] = $matches[0];
+			return $key;
+		},
+		$html
+	);
+
+	$html = preg_replace_callback(
+		'/<(pre|textarea|code)\b[^>]*>.*?<\/\1>/is',
+		function ($matches) use (&$placeholders) {
+			$key = "__PLACEHOLDER_" . count($placeholders) . "__";
+			$placeholders[$key] = $matches[0];
+			return $key;
+		},
+		$html
+	);
+
+	$html = preg_replace('/\s*>\s+/', '> ', $html);
+	$html = preg_replace('/\s+</', ' <', $html);
+
+	$html = preg_replace_callback(
+		'/>(\s+)</',
+		function ($matches) {
+			return '> ' . trim($matches[1]) . ' <';
+		},
+		$html
+	);
+
+	$html = preg_replace('/\s+/', ' ', $html);
+	$html = preg_replace('/<!--(?!\[if).*?-->/', '', $html);
+
+	foreach ($placeholders as $key => $original) {
+		if (preg_match('/^<(script|style)/i', $original, $tag)) {
+			if (stripos($tag[1], 'script') !== false) {
+				$original = preg_replace_callback(
+					'/<script(\b[^>]*)>(.*?)<\/script>/is',
+					function ($matches) {
+						return "<script" . $matches[1] . ">" . minifyJS($matches[2]) . "</script>";
+					},
+					$original
+				);
+			}
+			elseif (stripos($tag[1], 'style') !== false) {
+				$original = preg_replace_callback(
+					'/<style(\b[^>]*)>(.*?)<\/style>/is',
+					function ($matches) {
+						return "<style" . $matches[1] . ">" . minifyCSS($matches[2]) . "</style>";
+					},
+					$original
+				);
+			}
+		}
+		$html = str_replace($key, $original, $html);
+	}
+
+	return trim($html);
+}
